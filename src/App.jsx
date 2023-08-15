5df694fb6a0ba672575ea6bb53c31b6b7839b304
MakkiAnjum <muhammadmakkianjum@yahoo.com>
2023-08-15 19:09:30 +0100

    Color change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { Suspense } from "react";
-import { BarLoader } from "react-spinners";
-import Home from "./pages/Home";
+import React, { useState, useEffect } from 'react';
+import { Suspense } from 'react';
+import { BarLoader } from 'react-spinners';
+import Home from './pages/Home';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -17,13 +17,13 @@ const App = () => {
     <Suspense
       fallback={
         <div className="h-screen w-full flex items-center justify-center">
-          <BarLoader color="#f6c400" height={4} />
+          <BarLoader color="#26abed" height={4} />
         </div>
       }
     >
       {loading ? (
         <div className="h-screen w-full flex items-center justify-center">
-          <BarLoader color="#f6c400" height={4} />
+          <BarLoader color="#26abed" height={4} />
         </div>
       ) : (
         <Home />
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
